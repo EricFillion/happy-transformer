10a7f91355107dfedaac81eb74e1381eea080ae0
LoganRoth <logantheroth@gmail.com>
2019-11-05 17:36:46 -0500

    WIP: Working on getting BERT to select from a list of options
    was able to completely reuse Umur's code, wondering if I am missing some intricacy here, both of the predict functions are identical for XLNet and BERT (seems incorrect). Also moved some funcitons around to reduce reusing code and promote modularity.

--- happy_transformer/happy_bert.py
+++ happy_transformer/happy_bert.py
@@ -3,6 +3,7 @@
 
 import torch
 from transformers import BertForMaskedLM, BertTokenizer
+import numpy as np
 
 from happy_transformer.happy_transformer import HappyTransformer
 
@@ -34,20 +35,8 @@ class HappyBERT(HappyTransformer):
 
         # TODO: medium: make it more modular
 
-        # Generated formatted text so that it can be tokenized. Mainly, add the required tags
-        formatted_text = self._HappyTransformer__get_formatted_text(text)
-        tokenized_text = self.tokenizer.tokenize(formatted_text)
-
-        masked_index = self._HappyTransformer__get_prediction_index(tokenized_text)
-        segments_ids = self._HappyTransformer__get_segment_ids(tokenized_text)
-        indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
-
-        # Convert inputs to PyTorch tensors
-        tokens_tensor = torch.tensor([indexed_tokens])
-        segments_tensors = torch.tensor([segments_ids])
-
-        tokens_tensor = tokens_tensor.to(self.gpu_support)
-        segments_tensors = segments_tensors.to(self.gpu_support)
+        tokens_tensor, segments_tensors, masked_index =\
+            self._HappyTransformer__get_tensors_and_mask_idx(text)
 
         with torch.no_grad():
             outputs = self.transformer(tokens_tensor, token_type_ids=segments_tensors)
@@ -68,3 +57,40 @@ class HappyBERT(HappyTransformer):
 
             return prediction_token[0], prediction_softmax
 
+    def predict_mask_with_options(self, text: str, options: list):
+
+        tokens_tensor, segments_tensors, masked_index =\
+            self._HappyTransformer__get_tensors_and_mask_idx(text)
+
+        with torch.no_grad():
+            outputs = self.transformer(tokens_tensor, token_type_ids=segments_tensors)
+            predictions = outputs[0]
+
+            softmax = self._HappyTransformer__softmax(predictions)[0]
+
+            option_ids = [self.tokenizer.encode(option) for option in options]
+
+            option_probs = list(map(lambda x: self.soft_sum(x, softmax, masked_index), option_ids))
+            tupled_option = tuple(zip(options, option_probs))
+            ranked_scores = sorted(tupled_option, key=lambda x: x[1], reverse=True)
+
+            if self.gpu_support == "cuda":
+                torch.cuda.empty_cache()
+            ranked_scores = self._HappyTransformer__format_option_scores(ranked_scores)
+            return ranked_scores
+
+    def soft_sum(self, option: list, softed, mask_id: int):
+        # TODO: Better logic.
+        """
+        Adds the softmax of a single option
+        XLNET tokenizer sometimes splits words in to pieces.
+        Ex: The councilmen -> ['the', 'council', 'men']
+        Pretty sure that this is mathematically wrong
+        :param option: Id of tokens in one option
+        :param softed: softmax of the output
+        :param mask: Index of masked word
+        :return: float Tensor
+        """
+        # Collects the softmax of all tokens in list
+        options = [softed[mask_id][op] for op in option]
+        return np.sum(options)
--- happy_transformer/happy_transformer.py
+++ happy_transformer/happy_transformer.py
@@ -57,7 +57,7 @@ class HappyTransformer:
             if token == self.masked_token:
                 return location
             location += 1
-        print("Error, {} not found in the input".format(self.mask_token))
+        print("Error, {} not found in the input".format(self.masked_token))
         return None # TODO: medium: find a proper way to deal with errors
 
     def __get_formatted_text(self, text):
@@ -142,3 +142,27 @@ class HappyTransformer:
 
             text = text + predict_word
         return text
+
+    def __get_tensors_and_mask_idx(self, text):
+        formatted_text = self.__get_formatted_text(text)
+        tokenized_text = self.tokenizer.tokenize(formatted_text)
+
+        masked_index = self.__get_prediction_index(tokenized_text)
+        segments_ids = self.__get_segment_ids(tokenized_text)
+        indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
+
+        # Convert inputs to PyTorch tensors
+        tokens_tensor = torch.tensor([indexed_tokens]).to(self.gpu_support)
+        segments_tensors = torch.tensor([segments_ids]).to(self.gpu_support)
+        return tokens_tensor, segments_tensors, masked_index
+
+    def __format_option_scores(self, ranked_scores: list):
+        """
+        :param: ranked_scores: list of tuples to be converted into user friendly dicitonary
+        :return: formatted_ranked_scores: list of dictionaries of the ranked scores
+        """
+        # TODO: Shouldn't depend on ranked_scores to already be in order
+        formatted_ranked_scores = list()
+        for word, score in ranked_scores:
+            formatted_ranked_scores.append({'word': word, 'score': score})
+        return formatted_ranked_scores
