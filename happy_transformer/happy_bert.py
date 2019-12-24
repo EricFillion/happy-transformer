5152b7aa39063f2576e8964da58d12bd56069d61
uGokalp <15ug1@queensu.ca>
2019-12-24 22:58:41 +0300

    Staticmethod

--- happy_transformer/happy_bert.py
+++ happy_transformer/happy_bert.py
@@ -20,7 +20,7 @@ class HappyBERT(HappyTransformer):
 
     def __init__(self, model='bert-large-uncased', initial_transformers=[]):
         super().__init__(model, initial_transformers)
-        self.mlm = None  # Masked Language Model
+        self.model = None  # Masked Language Model
         self.nsp = None  # Next Sentence Prediction
         self.tokenizer = BertTokenizer.from_pretrained(model)
         self.masked_token = self.tokenizer.mask_token
@@ -32,8 +32,8 @@ class HappyBERT(HappyTransformer):
         """
         Initializes the BertForMaskedLM transformer
         """
-        self.mlm = BertForMaskedLM.from_pretrained(self.model_to_use)
-        self.mlm.eval()
+        self.model = BertForMaskedLM.from_pretrained(self.model_to_use)
+        self.model.eval()
 
     def _get_next_sentence_prediction(self):
         """
@@ -48,19 +48,21 @@ class HappyBERT(HappyTransformer):
         """
         return super()._get_prediction_softmax(text)
 
-    def fine_tune(self, train_path, test_path):
+    @staticmethod
+    def fine_tune(train_path, test_path):
         logger = logging.getLogger(__name__)
-        self._get_masked_language_model()
+        model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                             datefmt='%m/%d/%Y %H:%M:%S',
                             level=logging.INFO)
         device = torch.device("cuda" if torch.cuda.is_available()
                               else "cpu")
-        self.mlm.resize_token_embeddings(len(self.tokenizer))
-        self.mlm.cuda()
-        train_dataset = load_and_cache_examples(self.tokenizer, file_path=train_path)
+        model.resize_token_embeddings(len(tokenizer))
+        model.cuda()
+        train_dataset = load_and_cache_examples(tokenizer, file_path=train_path)
         logger.info("Training Started")
-        global_step, tr_loss = train(train_dataset, self.mlm, self.tokenizer)
+        global_step, tr_loss = train(train_dataset, model, tokenizer)
         logger.info(" global_step = %s, average loss = %s", global_step, tr_loss)
         # Start Eval
         logger.info("Eval Started")
