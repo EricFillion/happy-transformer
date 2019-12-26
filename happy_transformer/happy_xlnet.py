76badfa725605890a2d213ffa649891aa7b39130
Eric Fillion <ericfillioncanada@gmail.com>
2019-12-26 15:32:03 -0500

    moved seq_class methods to HappyTransformer

--- happy_transformer/happy_xlnet.py
+++ happy_transformer/happy_xlnet.py
@@ -1,4 +1,3 @@
-# pylint: disable=W0511
 from transformers import XLNetLMHeadModel, XLNetTokenizer
 
 from happy_transformer.happy_transformer import HappyTransformer
@@ -24,23 +23,15 @@ class HappyXLNET(HappyTransformer):
     Implementation of XLNET for masked word prediction
     """
 
-    def __init__(self, model='xlnet-large-cased', initial_transformers=[]):
+    def __init__(self, model='xlnet-base-cased', initial_transformers=[]):
         super().__init__(model, initial_transformers)
         self.mlm = None
-        self.seq = None
-        self.model_version = model
         self.tokenizer = XLNetTokenizer.from_pretrained(model)
         self.masked_token = self.tokenizer.mask_token
         self.sep_token = self.tokenizer.sep_token
         self.cls_token = self.tokenizer.cls_token
         self.model = 'XLNET'
         self.classifier_name = ""
-
-
-
-        self.seq_args = classifier_args.copy()
-        self.seq_trained = False
-
 
     def _get_masked_language_model(self):
         """
@@ -49,47 +40,4 @@ class HappyXLNET(HappyTransformer):
         self.mlm = XLNetLMHeadModel.from_pretrained(self.model_to_use)
         self.mlm.eval()
 
-
-    def _init_sequence_classifier(self, classifier_name: str):
-        self.classifier_name = classifier_name
-        self.seq_args['classifier_name'] = classifier_name
-        self.seq_args['model_name'] = classifier_name
-        self.seq_args['model_name'] = self.model_version
-        self.seq_args['output_dir'] = "outputs/" + classifier_name
-        self.seq_args['cache_dir'] = "cache/" + classifier_name
-        self.seq_args['data_dir'] = "data/" + classifier_name
-        self.seq = SequenceClassifier(self.seq_args, self.tokenizer)
-        self.seq.tokenizer = self.tokenizer
-        print(self.classifier_name, "has been initialized")
-
-
-    def _train_sequence_classifier(self, train_df):
-        if self.seq == None:
-            print("First initialize the sequence classifier")
-            return
-        #  self.seq.train_tsv = train_df.to_csv(sep='\t', index=True, header=False, columns=train_df.columns)
-        train_df = train_df.astype("str")
-        self.seq.train_list_data = train_df.values.tolist()
-
-        self.seq_args["do_train"] = True
-        self.seq.run_sequence_classifier()
-        self.seq_args["do_train"] = False
-        self.seq_trained = True
-
-        print("Training for ", self.classifier_name, "has been completed")
-
-
-    def _eval_sequence_classifier(self, eval_df):
-        if self.seq_trained == False:
-            print("First train the sequence classifier")
-            return
-        eval_df = eval_df.astype("str")
-
-
-        self.seq.eval_list_data = eval_df.values.tolist()
-
-        self.seq_args["do_eval"] = True
-        self.seq.run_sequence_classifier()
-        self.seq_args["do_eval"] = False
-        print("Evaluation for ", self.classifier_name, "has been completed")
 
