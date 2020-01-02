7426a0edde136a5b5b8466ab07dc7bc5cfed3871
uGokalp <15ug1@queensu.ca>
2020-01-02 17:23:52 +0300

    FinetuneBert class implemented

--- happy_transformer/bert_utils.py
+++ happy_transformer/bert_utils.py
@@ -26,9 +26,11 @@ class TextDataset(Dataset):
         with open(file_path, encoding="utf-8") as f:
             text = f.read()
 
-        tokenized_text = tokenizer.encode(text, add_special_tokens=True)  # Get ids from text
+        tokenized_text = tokenizer.encode(
+            text, add_special_tokens=True)  # Get ids from text
         self.examples = []
-        for i in range(0, len(tokenized_text) - block_size + 1, block_size):  # Truncate examples to a max blocksize
+        # Truncate examples to a max blocksize
+        for i in range(0, len(tokenized_text) - block_size + 1, block_size):
             self.examples.append(tokenized_text[i:i + block_size])
 
     def __len__(self):
@@ -57,20 +59,26 @@ def mask_tokens(inputs, tokenizer):
     """
     labels = inputs.clone()
     # We sample a few tokens in each sequence for masked-LM training (with probability args.mlm_probability defaults to 0.15 in Bert/RoBERTa)
-    probability_matrix = torch.full(labels.shape, 0.15)  # MLM Prob is 0.15 in examples
+    # MLM Prob is 0.15 in examples
+    probability_matrix = torch.full(labels.shape, 0.15)
     special_tokens_mask = [tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in
                            labels.tolist()]
-    probability_matrix.masked_fill_(torch.tensor(special_tokens_mask, dtype=torch.bool), value=0.0)
+    probability_matrix.masked_fill_(torch.tensor(
+        special_tokens_mask, dtype=torch.bool), value=0.0)
     masked_indices = torch.bernoulli(probability_matrix).bool()
     labels[~masked_indices] = -1  # We only compute loss on masked tokens
 
     # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
-    indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
-    inputs[indices_replaced] = tokenizer.convert_tokens_to_ids(tokenizer.mask_token)
+    indices_replaced = torch.bernoulli(torch.full(
+        labels.shape, 0.8)).bool() & masked_indices
+    inputs[indices_replaced] = tokenizer.convert_tokens_to_ids(
+        tokenizer.mask_token)
 
     # 10% of the time, we replace masked input tokens with random word
-    indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
-    random_words = torch.randint(len(tokenizer), labels.shape, dtype=torch.long)
+    indices_random = torch.bernoulli(torch.full(
+        labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+    random_words = torch.randint(
+        len(tokenizer), labels.shape, dtype=torch.long)
     inputs[indices_random] = random_words[indices_random]
 
     # The rest of the time (10% of the time) we keep the masked input tokens unchanged
@@ -91,7 +99,8 @@ def train(model, tokenizer, train_dataset, batch_size, lr, adam_epsilon, epochs)
     """
 
     train_sampler = RandomSampler(train_dataset)
-    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=batch_size)
+    train_dataloader = DataLoader(
+        train_dataset, sampler=train_sampler, batch_size=batch_size)
 
     t_total = len(train_dataloader) // batch_size  # Total Steps
 
@@ -100,10 +109,12 @@ def train(model, tokenizer, train_dataset, batch_size, lr, adam_epsilon, epochs)
     optimizer_grouped_parameters = [
         {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
          'weight_decay': 0.01},
-        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        {'params': [p for n, p in model.named_parameters() if any(
+            nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
     optimizer = AdamW(optimizer_grouped_parameters, lr=lr, eps=adam_epsilon)
-    scheduler = get_linear_schedule_with_warmup(optimizer, warmup_steps=0, t_total=t_total)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, warmup_steps=0, t_total=t_total)
 
     # ToDo Case for fp16
 
@@ -126,7 +137,8 @@ def train(model, tokenizer, train_dataset, batch_size, lr, adam_epsilon, epochs)
             labels = labels.to('cuda')
 
             outputs = model(inputs, masked_lm_labels=labels)
-            loss = outputs[0]  # model outputs are always tuple in transformers (see doc)
+            # model outputs are always tuple in transformers (see doc)
+            loss = outputs[0]
 
             loss.backward()
             tr_loss += loss.item()
@@ -183,7 +195,8 @@ def create_dataset(tokenizer, file_path, block_size=512):
     :param block_size: Should be in range of [0,512], viable choices are 64, 128, 256, 512
     :return: The dataset
     """
-    dataset = TextDataset(tokenizer, file_path=file_path, block_size=block_size)
+    dataset = TextDataset(tokenizer, file_path=file_path,
+                          block_size=block_size)
     return dataset
 
 
@@ -197,7 +210,8 @@ def evaluate(model, tokenizer, eval_dataset, batch_size):
     :return: The perplexity of the dataset
     """
     eval_sampler = SequentialSampler(eval_dataset)  # Same order samplinng
-    eval_dataloader = DataLoader(eval_dataset, sampler=eval_sampler, batch_size=batch_size)
+    eval_dataloader = DataLoader(
+        eval_dataset, sampler=eval_sampler, batch_size=batch_size)
 
     # Eval!
     logger.info("***** Running evaluation *****")
@@ -232,3 +246,54 @@ def evaluate(model, tokenizer, eval_dataset, batch_size):
         logger.info("  %s = %s", key, str(result[key]))
 
     return result
+
+
+class FinetuneBert():
+
+    """
+
+    :param train_path: Path to the training file, expected to be a .txt or similar
+    :param test_path: Path to the testing file, expected to be a .txt or similar
+
+    Default parameters for effortless finetuning
+    batch size = 1
+    Number of epochs  = 1
+    Learning rate = 5e-5
+    Adam epsilon = 1e-8
+    Model = 'bert-base-uncased'
+
+    """
+
+    def __init__(self, train_path, test_path, batch_size=1, epochs=1, lr=5e-5, adam_epsilon=1e-8,
+                 model_name='bert-base-uncased'):
+
+        self.model = BertForMaskedLM.from_pretrained(model_name)
+        self.tokenizer = BertTokenizer.from_pretrained(model_name)
+        self.train_path = train_path
+        self.test_path = test_path
+
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.lr = 5e-5
+        self.adam_epsilon = 1e-8
+
+        self.result = None
+
+    def train(self):
+        self.model.resize_token_embeddings(len(self.tokenizer))
+        # Start Train
+        self.model.cuda()
+        train_dataset = create_dataset(
+            self.tokenizer, file_path=self.train_path)
+        train(self.model, self.tokenizer, train_dataset, batch_size=self.batch_size,
+              epochs=self.epochs, lr=self.lr, adam_epsilon=self.adam_epsilon)
+
+        del train_dataset
+
+        # Start Eval
+        model, tokenizer = switch_to_new('model')
+        model.cuda()
+        test_dataset = create_dataset(self.tokenizer, file_path=self.test_path)
+        self.result = evaluate(model, tokenizer, test_dataset, batch_size=2)
+        del test_dataset
+        print("Result saved to self.result")
