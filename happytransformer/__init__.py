761bc33a89534bd90ff41714b6a4bd79d377c0f2
LoganRoth <logantheroth@gmail.com>
2020-01-07 20:30:57 -0500

    Changing import sequence

--- happytransformer/__init__.py
+++ happytransformer/__init__.py
@@ -1,5 +1,6 @@
-from happytransformer.happy_bert import HappyBERT
-from happytransformer.happy_xlnet import HappyXLNET
+import happytransformer.sequence_classifier
 from happytransformer.happy_roberta import HappyROBERTA
+from happytransformer.happy_xlnet import HappyXLNET
+from happytransformer.happy_bert import HappyBERT
 
 name = "happytransformer"
