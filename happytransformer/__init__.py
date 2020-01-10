bc3beb3c840724a0ec89bbc6bfe70065a31f0584
Eric Fillion <ericfillioncanada@gmail.com>
2020-01-10 14:50:28 -0500

    Added classifier args to __init__.py

--- happytransformer/__init__.py
+++ happytransformer/__init__.py
@@ -1,5 +1,6 @@
 from happytransformer.happy_roberta import HappyROBERTA
 from happytransformer.happy_xlnet import HappyXLNET
 from happytransformer.happy_bert import HappyBERT
+from happytransformer.classifier_args import classifier_args
 
 name = "happytransformer"
