ffe306ede87fa3f47f4c7f2b1c69c1b8e2bb135c
Ted Brownlow <ted.brownlow@gmail.com>
2020-10-12 17:26:51 -0400

    expanded test

--- tests/next_sentence_probs.py
+++ tests/next_sentence_probs.py
@@ -1,12 +1,20 @@
 from happytransformer import HappyBERT
 
+def percent(x):
+    return f"{x*100:.2f}%"
+
 sentence_pairs = [
-    ["How old are you?","The Eiffel Tower is in Paris"],
-    ["How old are you?","I am 40 years old"]
+    ["How old are you?","The Eiffel Tower is in Paris",False],
+    ["How old are you?","I am 40 years old",True]
 ]
 
 happy = HappyBERT()
-for a,b in sentence_pairs:
+for a,b,follows in sentence_pairs:
+    print('==============================')
     print(a)
     print(b)
-    print(happy.next_sentence_probability(a,b))
+    p = happy.next_sentence_probability(a,b)
+    assert p>=0 and p<=1
+    predict_follows = p > 0.5
+    assert predict_follows == follows
+    print(percent(p))
