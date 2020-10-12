d45e8c2b1d875bb47f37553f03024ebe159e6e4a
Ted Brownlow <ted.brownlow@gmail.com>
2020-10-12 17:38:02 -0400

    reformatted test

--- tests/next_sentence_probs.py
+++ tests/next_sentence_probs.py
@@ -1,5 +1,6 @@
 from happytransformer import HappyBERT
 
+# formats a float as a percentage
 def percent(x):
     return f"{x*100:.2f}%"
 
@@ -17,19 +18,25 @@ sentence_pairs = [
     ["How old are you?","I am 40 years old",True]
 ]
 
-def nsp_with_multiple_sentences():
-    happy.next_sentence_probability(
-        "This is the first sentence. This is the second sentence",
-        "An error should be raised here"
-    )
-assert_errors(nsp_with_multiple_sentences)
+def test_argument_errors():
+    two_sentences = "This is the first sentence. This is the second sentence"
+    one_sentence = "This is one sentence."
+    assert_errors(lambda: happy.predict_next_sentence(two_sentences,one_sentence))
+    assert_errors(lambda: happy.predict_next_sentence(one_sentence,two_sentences))
+    assert_errors(lambda: happy.next_sentence_probability(one_sentence,two_sentences))
+    assert_errors(lambda: happy.next_sentence_probability(two_sentences,one_sentence))
+test_argument_errors()
 
-for a,b,follows in sentence_pairs:
-    print('==============================')
-    print(a)
-    print(b)
-    p = happy.next_sentence_probability(a,b)
-    assert p>=0 and p<=1
-    predict_follows = p > 0.5
-    assert predict_follows == follows
-    print(percent(p))
+def test_nsp():
+    for a,b,follows in sentence_pairs:
+        print('==============================')
+        print(a)
+        print(b)
+        p = happy.next_sentence_probability(a,b)
+        assert p>=0 and p<=1
+        predict_follows = p > 0.5
+        assert predict_follows == follows
+
+        assert happy.predict_next_sentence(a,b) == follows
+        print(percent(p))
+test_nsp()
