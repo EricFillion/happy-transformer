28f5abf2f3ac192b4b8a1bd3ce4f84013ee195be
Eric Fillion <ericfillioncanada@gmail.com>
2020-12-13 00:24:57 -0500

    Changed reference from Pytorch to Hugging Face

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages = ['happytransformer',],
     version = '1.1.3',
     license='Apache 2.0',
-    description = "Happy Transformer is an API built on top of PyTorch's transformer library that makes it easy to utilize state-of-the-art NLP models.",
+    description = "Happy Transformer is an API built on top of Hugging Face's Transformer library that makes it easy to utilize state-of-the-art NLP models.",
     long_description= readme,
     long_description_content_type='text/markdown',
     author = "The Happy Transformer Development Team",
