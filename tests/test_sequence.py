b744fdeb5c138f5e923b3b32969eed2562f83f13
Ted Brownlow <ted.brownlow@gmail.com>
2020-12-18 17:32:58 -0500

    added comment

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -3,6 +3,10 @@ from happytransformer import HappyBERT
 happy = HappyBERT()
 
 def test_sequence_runs():
+    '''
+    run sequence classification end to end
+    only checks that no errors are raised
+    '''
     happy.init_sequence_classifier()
     happy.train_sequence_classifier('tests/test_sequence.csv')
     happy.eval_sequence_classifier('tests/test_sequence.csv')
