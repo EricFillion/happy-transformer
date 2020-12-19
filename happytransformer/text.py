bfc2f6961c362c3996638613043979cb2de18465
Ted Brownlow <ted.brownlow@gmail.com>
2020-12-19 00:10:08 -0500

    removed useless file

--- happytransformer/text.py
+++
@@ -1,9 +0,0 @@
-def remove_starting_character(self, options, starting_char):
-    new_predictions = list()
-    for prediction in options:
-        if prediction[0] == starting_char:
-            new_prediction = prediction[1:]
-            new_predictions.append(new_prediction)
-        else:
-            new_predictions.append(prediction)
-    return new_predictions
