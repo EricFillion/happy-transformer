2be2fa04f7e491a5d45d99162f331e19204dc4c7
Eric Fillion <ericfillioncanada@gmail.com>
2021-01-09 20:38:28 -0500

    Added linebreaks

--- tests/test_wp.py
+++ tests/test_wp.py
@@ -16,7 +16,8 @@ def test_mwp_top_k():
         "Please pass the salt and [MASK]",
         top_k=2
     )
-    answer = [WordPredictionResult(token_str='pepper', score=0.2664579749107361), WordPredictionResult(token_str='vinegar', score=0.08760260790586472)]
+    answer = [WordPredictionResult(token_str='pepper', score=0.2664579749107361),
+              WordPredictionResult(token_str='vinegar', score=0.08760260790586472)]
 
     assert result == answer
 
@@ -26,7 +27,8 @@ def test_mwp_targets():
         "Please pass the salt and [MASK]",
         targets=["water", "spices"]
     )
-    answer = [WordPredictionResult(token_str='water', score=0.014856964349746704), WordPredictionResult(token_str='spices', score=0.009040987119078636)]
+    answer = [WordPredictionResult(token_str='water', score=0.014856964349746704),
+              WordPredictionResult(token_str='spices', score=0.009040987119078636)]
     assert result == answer
 
 
