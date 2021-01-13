8280f4f6f7369e5acc3064b186a0c2a8ba53234b
Eric Fillion <ericfillioncanada@gmail.com>
2021-01-12 22:21:48 -0500

    Better use of split arg

--- examples/question_answering/training_example.py
+++ examples/question_answering/training_example.py
@@ -8,11 +8,11 @@ def main():
     train_csv_path = "train.csv"
     eval_csv_path = "eval.csv"
 
-    train_dataset = load_dataset('squad', split='train')
-    eval_dataset = load_dataset('squad', split='validation')
+    train_dataset = load_dataset('squad', split='train[0:499]')
+    eval_dataset = load_dataset('squad', split='validation[0:99]')
 
-    generate_csv(train_csv_path, train_dataset, 500)
-    generate_csv(eval_csv_path, eval_dataset, 100)
+    generate_csv(train_csv_path, train_dataset)
+    generate_csv(eval_csv_path, eval_dataset)
 
     happy_qa = HappyQuestionAnswering(model_type="BERT", model_name="bert-base-uncased")
     before_loss = happy_qa.eval(eval_csv_path)
@@ -24,21 +24,16 @@ def main():
     assert after_loss < before_loss
 
 
-def generate_csv(csv_path, dataset, count):
+def generate_csv(csv_path, dataset):
     with open(csv_path, 'w', newline='') as csvfile:
         writter = csv.writer(csvfile)
         writter.writerow(["context", "question", "answer_text", "answer_start"])
-        i = 0
         for case in dataset:
             context = case["context"]
             question = case["question"]
             answer_text = case["answers"]["text"][0]
             answer_start = case["answers"]["answer_start"][0]
             writter.writerow([context, question, answer_text, answer_start])
-
-            i += 1
-            if i == count:
-                break
 
 
 if __name__ == "__main__":
