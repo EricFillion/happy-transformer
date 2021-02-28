cd4021c79fa34f547e0f0fef40da13161e834b7e
Eric Fillion <ericfillioncanada@gmail.com>
2021-02-28 00:10:08 -0500

    changed output type for TOC to list of dataclasses

--- happytransformer/happy_token_classification.py
+++ happytransformer/happy_token_classification.py
@@ -8,9 +8,16 @@ from happytransformer.toc.trainer import TOCTrainer
 from happytransformer.cuda_detect import detect_cuda_device_number
 from happytransformer.adaptors import get_adaptor
 
+
 @dataclass
 class TokenClassificationResult:
-    tokens: List[str]
+    word: str
+    score: float
+    entity: str
+    index: int
+    start: int
+    end: int
+
 
 class HappyTokenClassification(HappyTransformer):
     """
@@ -31,10 +38,8 @@ class HappyTokenClassification(HappyTransformer):
 
         self._trainer = TOCTrainer(self.model, model_type, self.tokenizer, self._device, self.logger)
 
-
-    def classify_token(self, text: str) -> str:
+    def classify_token(self, text: str) -> List[TokenClassificationResult]:
         """
-
         :param text: Text that contains tokens to be classified
         :return:
         """
@@ -42,7 +47,17 @@ class HappyTokenClassification(HappyTransformer):
             raise ValueError('the "text" argument must be a single string')
         results = self._pipeline(text)
 
-        return results
+        return [
+            TokenClassificationResult(
+                word=answer["word"],
+                score=answer["score"],
+                entity=answer["entity"],
+                index=answer["index"],
+                start=answer["start"],
+                end=answer["end"],
+            )
+            for answer in results
+        ]
 
 
     def train(self, input_filepath, args):
