243198555403bfd26cc87144d48b4a2b024cb20a
Eric Fillion <ericfillioncanada@gmail.com>
2021-04-24 17:49:00 -0400

    Added parentheses to multi line import

--- happytransformer/__init__.py
+++ happytransformer/__init__.py
@@ -10,7 +10,13 @@ from happytransformer.qa.default_args import ARGS_QA_TRAIN, ARGS_QA_EVAl
 from happytransformer.tc.default_args import ARGS_TC_TRAIN, ARGS_TC_EVAL
 from happytransformer.wp.default_args import ARGS_WP_TRAIN, ARGS_WP_EVAl
 
-from happytransformer.happy_generation import HappyGeneration, GEN_DEFAULT_SETTINGS, GEN_GREEDY_SETTINGS, \
-    GEN_BEAM_SETTINGS, GEN_TOP_K_SAMPLING_SETTINGS, GEN_GENERIC_SAMPLING_SETTINGS
+from happytransformer.happy_generation import (
+    HappyGeneration,
+    GEN_DEFAULT_SETTINGS,
+    GEN_GREEDY_SETTINGS,
+    GEN_BEAM_SETTINGS,
+    GEN_TOP_K_SAMPLING_SETTINGS,
+    GEN_GENERIC_SAMPLING_SETTINGS,
+)
 
 name = "happytransformer"
