9de30fd8d80c8bfda61ae34aa74f7119f6efefad
Eric Fillion <ericfillioncanada@gmail.com>
2021-05-10 13:27:03 -0400

    fixed csv typo

--- examples/generation/training_example.py
+++ examples/generation/training_example.py
@@ -3,14 +3,14 @@ from happytransformer.happy_generation import HappyGeneration
 
 
 def main():
-    train_csv_path = "train.txt"
-    eval_csv_path = "eval.txt"
+    train_txt_path = "train.txt"
+    eval_txt_path = "eval.txt"
 
     train_dataset = load_dataset('billsum', split='train[0:1999]')
     eval_dataset = load_dataset('billsum', split='test[0:499]')
 
-    generate_txt(train_csv_path, train_dataset)
-    generate_txt(eval_csv_path, eval_dataset)
+    generate_txt(train_txt_path, train_dataset)
+    generate_txt(eval_txt_path, eval_dataset)
 
     happy_gen = HappyGeneration(model_type="GPT2", model_name="gpt2")
     starter_texts = ["Authorizes", "Allows", "Prevents", "Restricts"]
@@ -18,11 +18,11 @@ def main():
     print("Examples Before Training: ")
     produce_examples(starter_texts, happy_gen)
 
-    before_loss = happy_gen.eval(eval_csv_path)
+    before_loss = happy_gen.eval(eval_txt_path)
 
-    happy_gen.train(train_csv_path)
+    happy_gen.train(train_txt_path)
 
-    after_loss = happy_gen.eval(eval_csv_path)
+    after_loss = happy_gen.eval(eval_txt_path)
 
     print("Before loss: ", before_loss.loss)
     print("After loss: ", after_loss.loss, end="\n\n")
@@ -31,8 +31,8 @@ def main():
     produce_examples(starter_texts, happy_gen)
 
 
-def generate_txt(csv_path, dataset):
-    with open(csv_path, 'w', newline='') as text_file:
+def generate_txt(txt_path, dataset):
+    with open(txt_path, 'w', newline='') as text_file:
         for case in dataset:
             text = case["summary"]
             text_file.write(text + "\n")
