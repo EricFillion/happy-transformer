939fd337561fb1f2e104fc8ae9b84214b8f92604
Eric Fillion <ericfillioncanada@gmail.com>
2021-06-12 17:24:04 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ readme = (current_location / "README.md").read_text()
 setup(
     name = 'happytransformer',
     packages = find_packages(),
-    version = '2.2.2',
+    version = '2.2.3',
     license='Apache 2.0',
     description = "Happy Transformer is an API built on top of Hugging Face's Transformer library that makes it easy to utilize state-of-the-art NLP models.",
     long_description= readme,
