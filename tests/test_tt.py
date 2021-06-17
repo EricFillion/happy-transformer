359f12dbc5e1e8c374ca0a678ab17be821d8f757
Eric Fillion <ericfillioncanada@gmail.com>
2021-06-17 01:45:37 -0400

    Renamed happy_gen to happy_tt

--- tests/test_tt.py
+++ tests/test_tt.py
@@ -3,48 +3,48 @@ from happytransformer import HappyTextToText, TextToTextSettings
 
 
 def test_default_simple():
-    happy_gen = HappyTextToText()
-    output = happy_gen.generate_text("translate English to French: Hello my name is Eric")
+    happy_tt = HappyTextToText()
+    output = happy_tt.generate_text("translate English to French: Hello my name is Eric")
     assert type(output.text) == str
 
 def test_default_min_max_length():
-    happy_gen = HappyTextToText()
+    happy_tt = HappyTextToText()
     args = TextToTextSettings(min_length=5, max_length=5)
-    output = happy_gen.generate_text("translate English to French: Hello my name is Eric", args=args)
-    tokens = happy_gen.tokenizer.encode(output.text, return_tensors="pt")
+    output = happy_tt.generate_text("translate English to French: Hello my name is Eric", args=args)
+    tokens = happy_tt.tokenizer.encode(output.text, return_tensors="pt")
     length = len(tokens[0])
     assert length == 5
 
 def test_all_methods():
-    happy_gen = HappyTextToText()
+    happy_tt = HappyTextToText()
 
     greedy_settings = TextToTextSettings(min_length=5, max_length=5, no_repeat_ngram_size=2)
-    output_greedy = happy_gen.generate_text(
+    output_greedy = happy_tt.generate_text(
         "translate English to French: Hello my name is Eric",
         args=greedy_settings)
 
 
     beam_settings = TextToTextSettings(min_length=5, max_length=5, early_stopping=True, num_beams=5)
 
-    output_beam_search = happy_gen.generate_text(
+    output_beam_search = happy_tt.generate_text(
         "translate English to French: Hello my name is Eric",
         args=beam_settings)
 
     generic_sampling_settings = TextToTextSettings(min_length=5, max_length=5, do_sample=True, early_stopping=False, top_k=0, temperature=0.7)
 
-    output_generic_sampling = happy_gen.generate_text(
+    output_generic_sampling = happy_tt.generate_text(
         "translate English to French: Hello my name is Eric",
         args=generic_sampling_settings)
 
     top_k_sampling_settings = TextToTextSettings(min_length=5, max_length=5, do_sample=True, early_stopping=False, top_k=50, temperature=0.7)
 
-    output_top_k_sampling = happy_gen.generate_text(
+    output_top_k_sampling = happy_tt.generate_text(
         "translate English to French: Hello my name is Eric",
         args=top_k_sampling_settings)
 
     top_p_sampling_settings = TextToTextSettings(min_length=5, max_length=5, do_sample=True, early_stopping=False, top_k=0, top_p=0.8, temperature=0.7)
 
-    output_top_p_sampling = happy_gen.generate_text(
+    output_top_p_sampling = happy_tt.generate_text(
         "translate English to French: Hello my name is Eric",
         args=top_p_sampling_settings)
 
