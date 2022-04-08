35f6f5c0dab6d74c7e4527eb2ab957138acac196
swcrazyfan <79735340+swcrazyfan@users.noreply.github.com>
2022-04-08 15:32:48 +0800

    Update cuda_detect.py

--- happytransformer/cuda_detect.py
+++ happytransformer/cuda_detect.py
@@ -1,4 +1,9 @@
 import torch
+import torch_xla
+import torch_xla.core.xla_model as xm
 
 def detect_cuda_device_number():
     return torch.cuda.current_device() if torch.cuda.is_available() else -1
+
+def detect_tpu_device_number():
+    return xm.xla_device().index if xm.xla_device() else -1
