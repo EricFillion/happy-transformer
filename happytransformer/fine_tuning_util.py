0c636d6e33187eedded0d1c9632a134583a763bc
swcrazyfan <79735340+swcrazyfan@users.noreply.github.com>
2022-04-08 15:35:14 +0800

    Update fine_tuning_util.py

--- happytransformer/fine_tuning_util.py
+++ happytransformer/fine_tuning_util.py
@@ -7,6 +7,120 @@ and
 https://github.com/huggingface/transformers/blob/master/examples/pytorch/language-modeling/run_clm.py
 """
 
+import torch_xla.core.xla_model as xm
+import torch_xla.distributed.parallel_loader as pl
+import torch_xla.distributed.xla_multiprocessing as xmp
+import torch_xla.utils.utils as xu
+
+
+def get_tpu_device():
+    """
+    Returns the TPU device if it exists.
+    :return:
+    """
+    tpu_device = xm.xla_device()
+    return tpu_device
+
+
+def get_tpu_strategy():
+    """
+    Returns the TPU strategy if it exists.
+    :return:
+    """
+    tpu_strategy = xm.xla_distributed_strategy()
+    return tpu_strategy
+
+
+def get_tpu_parallel_loader():
+    """
+    Returns the TPU parallel loader if it exists.
+    :return:
+    """
+    tpu_parallel_loader = pl.ParallelLoader(xm.xla_distributed_strategy(), [])
+    return tpu_parallel_loader
+
+
+def get_tpu_multiprocessing():
+    """
+    Returns the TPU multiprocessing if it exists.
+    :return:
+    """
+    tpu_multiprocessing = xmp.spawn(get_tpu_device(), get_tpu_strategy(), get_tpu_parallel_loader())
+    return tpu_multiprocessing
+
+
+def get_tpu_device_ordinal():
+    """
+    Returns the TPU device ordinal if it exists.
+    :return:
+    """
+    tpu_device_ordinal = xm.get_ordinal()
+    return tpu_device_ordinal
+
+
+def get_tpu_device_count():
+    """
+    Returns the TPU device count if it exists.
+    :return:
+    """
+    tpu_device_count = xm.xrt_world_size()
+    return tpu_device_count
+
+
+def get_tpu_device_list():
+    """
+    Returns the TPU device list if it exists.
+    :return:
+    """
+    tpu_device_list = xu.get_devices()
+    return tpu_device_list
+
+
+def get_tpu_device_list_str():
+    """
+    Returns the TPU device list as a string if it exists.
+    :return:
+    """
+    tpu_device_list_str = xu.get_devices_str()
+    return tpu_device_list_str
+
+
+def get_tpu_device_list_str_short():
+    """
+    Returns the TPU device list as a short string if it exists.
+    :return:
+    """
+    tpu_device_list_str_short = xu.get_devices_str(short=True)
+    return tpu_device_list_str_short
+
+
+def get_tpu_device_list_str_long():
+    """
+    Returns the TPU device list as a long string if it exists.
+    :return:
+    """
+    tpu_device_list_str_long = xu.get_devices_str(long=True)
+    return tpu_device_list_str_long
+
+
+def get_tpu_device_list_str_short_long():
+    """
+    Returns the TPU device list as a short and long string if it exists.
+    :return:
+    """
+    tpu_device_list_str_short_long = xu.get_devices_str(short=True, long=True)
+    return tpu_device_list_str_short_long
+
+
+def get_tpu_device_list_str_long_short():
+    """
+    Returns the TPU device list as a long and short string if it exists.
+    :return:
+    """
+    tpu_device_list_str_long_short = xu.get_devices_str(long=True, short=True)
+    return tpu_device_list_str_long_short
+"""
+
 
 def preprocess_concatenate(tokenizer, dataset, preprocessing_processes, mlm=True):
     """
