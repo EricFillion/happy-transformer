d511417bb99ca206edd719d6a57b655f03f681d5
Eric Fillion <ericfillion1999@gmail.com>
2023-07-11 20:15:10 -0400

    Added test_eval_data

---
+++ tests/test_eval_data.py
@@ -0,0 +1,51 @@
+from happytransformer import (
+    HappyGeneration,
+    GENTrainArgs)
+import pytest
+
+DATA_PATH = "../data/gen/train-eval.txt"
+
+# tests what happens when no eval file is provided and eval_ratio is set to 0.
+def test_gen_eval_0():
+    happy_gen = HappyGeneration("GPT-2", "sshleifer/tiny-gpt2")
+
+    args = GENTrainArgs(eval_ratio=0.0, num_train_epochs=1)
+
+    with pytest.raises(ValueError):
+        happy_gen.train(DATA_PATH, args=args)
+
+def test_gen_eval_ratio():
+    happy_gen = HappyGeneration("GPT-2", "sshleifer/tiny-gpt2")
+
+    args = GENTrainArgs(eval_ratio=0.1, num_train_epochs=1)
+
+    happy_gen.train(DATA_PATH, args=args)
+
+
+def test_gen_eval_path():
+    happy_gen = HappyGeneration("GPT-2", "sshleifer/tiny-gpt2")
+
+    args = GENTrainArgs(eval_ratio=0.1, num_train_epochs=1)
+
+    happy_gen.train(DATA_PATH, args=args, eval_filepath=DATA_PATH)
+
+
+
+def test_gen_saved_data():
+    happy_gen = HappyGeneration("GPT-2", "sshleifer/tiny-gpt2")
+    save_path = "data/test/"
+    args_save = GENTrainArgs(eval_ratio=0.1, num_train_epochs=1, save_preprocessed_data=True, save_preprocessed_data_path=save_path)
+
+    happy_gen.train(DATA_PATH, args=args_save)
+
+    args_load = GENTrainArgs(num_train_epochs=1, load_preprocessed_data=True, load_preprocessed_data_path=save_path)
+
+    happy_gen.train(DATA_PATH, args=args_load)
+
+    # Should still work even if eval_filepath is provided
+    happy_gen.train(DATA_PATH, eval_filepath=DATA_PATH,  args=args_load)
+
+
+
+
+
