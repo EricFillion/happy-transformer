3466701ce8e3510b98f47df3271d1ea017ee61bc
Eric Fillion <ericfillion1999@gmail.com>
2023-08-01 23:10:24 -0400

    got rid of legacy imports

--- happytransformer/__init__.py
+++ happytransformer/__init__.py
@@ -14,22 +14,4 @@ from happytransformer.args import (
     TTTrainArgs, TTEvalArgs
 )
 
-# Legacy imports
-from happytransformer.legacy import (
-    ARGS_GEN_TRAIN,
-    ARGS_GEN_EVAl,
-    ARGS_QA_TRAIN,
-    ARGS_QA_EVAl,
-    ARGS_QA_TEST,
-    ARGS_SP_TRAIN,
-    ARGS_TC_TRAIN,
-    ARGS_TC_EVAL,
-    ARGS_TC_TEST,
-    ARGS_TOC_TRAIN,
-    ARGS_TOC_EVAl,
-    ARGS_TOC_TEST,
-    ARGS_WP_TRAIN,
-    ARGS_WP_EVAl)
-
-
 name = "happytransformer"
