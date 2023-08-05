b0697a4bd09918d5abbc46e58b6763db322cb4fc
Eric Fillion <ericfillion1999@gmail.com>
2023-08-05 15:07:02 -0400

    csv -> txt

--- examples/generation/training_example.py
+++ examples/generation/training_example.py
@@ -1,32 +1,31 @@
-import csv
 from datasets import load_dataset
 from happytransformer import HappyGeneration,  GENTrainArgs
 
 
 def main():
-    train_csv_path = "train.txt"
-    eval_csv_path = "eval.txt"
+    train_txt_path = "train.txt"
+    eval_txt_path = "eval.txt"
 
     train_dataset = load_dataset('billsum', split='train[0:1999]')
     eval_dataset = load_dataset('billsum', split='test[0:199]')
 
-    generate_txt(train_csv_path, train_dataset)
-    generate_txt(eval_csv_path, eval_dataset)
+    generate_txt(train_txt_path, train_dataset)
+    generate_txt(eval_txt_path, eval_dataset)
 
     happy_gen = HappyGeneration(model_type="GPT2", model_name="gpt2")
 
     train_args = GENTrainArgs(
         num_train_epochs=1,
         learning_rate=1e-5,
-        # fp16=True,
+        max_length=128,
+        fp16=True,
+        # deepspeed="ZERO-2",
         # report_to = ('wandb'),
         # project_name = "happy-transformer-examples",
         # run_name = "text-generation",
-        # deepspeed="ZERO-2",
-        # max_length = 128
     )
 
-    happy_gen.train(train_csv_path, args=train_args, eval_filepath=eval_csv_path)
+    happy_gen.train(train_txt_path, args=train_args, eval_filepath=eval_txt_path)
 
     happy_gen.save("finetuned-model/")
 
